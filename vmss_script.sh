e880a83cb88fd21c21b69e55da9c8f25ed3f14c3
przemykk <57070129+przemykk@users.noreply.github.com>
2020-03-27 16:41:25 +0100

    Update vmss_script.sh

--- vmss_script.sh
+++ vmss_script.sh
@@ -1,4 +1,5 @@
-!#/bin/bash
+#!/bin/bash
 
 sudo apt-get install stress -y
 sudo echo "This is the updated app installed on the Virtual Machine Scale Set !" > /var/www/html/index.html
+sudo stress --cpu 1
